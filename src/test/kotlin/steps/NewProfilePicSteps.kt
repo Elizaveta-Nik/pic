ba2784e15882d31ed807df65b7fb64b4005cfb08
Elizaveta <elizabeth.frisky@gmail.com>
2026-03-08 14:30:18 +0300

    add step and update test

---
+++ src/test/kotlin/steps/NewProfilePicSteps.kt
@@ -0,0 +1,19 @@
+package steps
+
+import org.openqa.selenium.WebDriver
+import tests.pages.NewProfilePicPage
+import tests.utils.LogParser
+
+internal class PhotoProcessingSteps(private val driver: WebDriver) {
+    private val page = NewProfilePicPage(driver)
+
+    fun openAndProcessPhoto(targetUrl: String, photoUrl: String) {
+        page.open(targetUrl)
+            .triggerPhotoProcessing(photoUrl)
+            .clickSaveAndShare()
+    }
+
+    fun getResultImageUrl(): String {
+        return LogParser.waitForDownloadUrl(driver)
+    }
+}
